180f1b976e422dd7acc15715da00b1a4c2920576
Ivan Kravchenko <41616785+TheKeyboardist@users.noreply.github.com>
2021-03-23 12:46:35 -0400

    swaping works
    however, any tiles can be swaped

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -4,99 +4,50 @@ using UnityEngine;
 
 public class GridController : MonoBehaviour
 {
+    public static GridController instance;
+
+
+    public List<Sprite> characters = new List<Sprite>();
+    public bool IsShifting { get; set; }
+
     [SerializeField]
-    public GameObject tilePrefab;
+    public GameObject tilePrefab;//will be working with this one
     public GameObject coverTile;
 
-    public int width;
-    public int height;
+    public int width;//needed 
+    public int height;//needed
 
-    Tile resourcesCentre;
-    public Tile[,] gridArray;
-    public GameObject[,] coverGrid;
-    public CoverTile[,] coverTileGrid;
+    Tile resourcesCentre;//won't be needed
+    public Tile[,] gridArray;//wont be needd
+    public GameObject[,] coverGrid;//wont be needed
+    public CoverTile[,] coverTileGrid;//wont be needed
 
     private void Awake()
     {
-        height = width = 16;
+        height = width = 8;
     }
 
     private void Start()
     {
+        instance = GetComponent<GridController>();
         SetUpGrid();
        // SetUpCoverGrid();
     }
 
 
 
-    void SetRandSpot()
-    {
-        int tempInt = Random.Range(0, width * height - 1);
-
-        for (int x = 0; x < gridArray.GetLength(0); x++)
-        {
-            for (int y = 0; y < gridArray.GetLength(1); y++)
-            {
-                if (gridArray[x, y].id == tempInt)
-                {
-                    var tileRenderer = gridArray[x, y].tile.GetComponent<Renderer>();
-                    tileRenderer.material.SetColor("_Color", new Color(0,1,0,1));
-
-
-                    gridArray[x, y].intensity = 100;
-                    gridArray[x, y].hasResources = true;
-                    gridArray[x, y].x = x;
-                    gridArray[x, y].y = y;
-
-                    resourcesCentre = gridArray[x, y];
-                }
-            }
-        }
-    }
-
-    void FillOutTiles(Tile tile)
-    {
-
-        for (int x = 0; x < gridArray.GetLength(0); x++)
-        {
-            for (int y = 0; y < gridArray.GetLength(1); y++)
-            {
-                if(!gridArray[x,y].hasResources)
-                {
-                    gridArray[x, y].intensity += 100 / (Mathf.Abs(tile.x - x) + Mathf.Abs(tile.y - y)) / 2;
-
-
-                    var tileRenderer = gridArray[x, y].tile.GetComponent<Renderer>();
-
-                    Color currentColor = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color");
-
-                    float currR = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color").r;
-                    float currG = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color").g;
-                    float currB = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color").b;
-
-
-                    float tempR = 1.0f / (Mathf.Abs(tile.x - x) + Mathf.Abs(tile.y - y)) / 2;
-                    float tempG = 0;
-                    float tempB = 1.0f / (Mathf.Abs(tile.x - x) + Mathf.Abs(tile.y - y)) / 2;
-
-                    tileRenderer.material.SetColor("_Color", new Color(currR - tempR, currG - tempG, currB - tempB));
-                }
-                  
-            }
-        }
-
-    }
-
-
     public void SetUpGrid()
     {
+        Sprite[] previousLeft = new Sprite[height];
+        Sprite previousBelow = null;
+
         if (gridArray != null)
         {
             for (int x = 0; x < gridArray.GetLength(0); x++)
             {
                 for (int y = 0; y < gridArray.GetLength(1); y++)
                 {
-                    Destroy(gridArray[x, y].tile);
+                    //Destroy(gridArray[x, y].tile);
                     gridArray[x, y] = null;
                 }
             }
@@ -119,64 +70,123 @@ public class GridController : MonoBehaviour
 
                     gridArray[x, y].x = x;
                     gridArray[x, y].y = y;
+
+                    List<Sprite> possibleCharacters = new List<Sprite>(); 
+                    possibleCharacters.AddRange(characters); 
+
+                    possibleCharacters.Remove(previousLeft[y]); 
+                    possibleCharacters.Remove(previousBelow);
+
+
+                    Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+                    temp.GetComponent<SpriteRenderer>().sprite = newSprite;
+
+                    previousLeft[y] = newSprite;
+                    previousBelow = newSprite;
+
+
                 }
             }
 
 
         for(int i = 0; i < (height*width)/51.2f;i++)
         {
-            SetRandSpot();
-            FillOutTiles(resourcesCentre);
+            // SetRandSpot();
+            //FillOutTiles(resourcesCentre);
         }
-
-        SetUpCoverGrid();
-
+        //SetUpCoverGrid();
     }
+    //void SetRandSpot()
+    //{
+    //    int tempInt = Random.Range(0, width * height - 1);
+
+    //    for (int x = 0; x < gridArray.GetLength(0); x++)
+    //    {
+    //        for (int y = 0; y < gridArray.GetLength(1); y++)
+    //        {
+    //            if (gridArray[x, y].id == tempInt)
+    //            {
+    //                var tileRenderer = gridArray[x, y].tile.GetComponent<Renderer>();
+    //                tileRenderer.material.SetColor("_Color", new Color(0,1,0,1));
 
 
+    //                gridArray[x, y].intensity = 100;
+    //                gridArray[x, y].hasResources = true;
+    //                gridArray[x, y].x = x;
+    //                gridArray[x, y].y = y;
 
-    public void SetUpCoverGrid()
-    {
-        if (coverTileGrid != null)
-        {
-            for (int x = 0; x < coverTileGrid.GetLength(0); x++)
-            {
-                for (int y = 0; y < coverTileGrid.GetLength(1); y++)
-                {
-                    Destroy(coverTileGrid[x, y].tile);
-                    coverTileGrid[x, y] = null;
-                }
-            }
-            coverTileGrid = null;
-        }
+    //                resourcesCentre = gridArray[x, y];
+    //            }
+    //        }
+    //    }
+    //}
 
+    //void FillOutTiles(Tile tile)
+    //{
 
-        coverTileGrid = new CoverTile[width, height];
-
-        int counter = 0;
-
-        for (int x = 0; x < coverTileGrid.GetLength(0); x++)
-        {
-            for (int y = 0; y < coverTileGrid.GetLength(1); y++)
-            {
-
-                GameObject temp = Instantiate(coverTile, new Vector3((x - (height / 2) + 0.5f)*0.5f, (y - (width / 2) + 0.5f) * 0.5f, 0), Quaternion.identity);
-                coverTileGrid[x, y] = temp.GetComponent<CoverTile>();
-                coverTileGrid[x, y].id = counter;
-                counter++;
-
-                coverTileGrid[x, y].x = x;
-                coverTileGrid[x, y].y = y;
-            }
-        }
-    }
+    //    for (int x = 0; x < gridArray.GetLength(0); x++)
+    //    {
+    //        for (int y = 0; y < gridArray.GetLength(1); y++)
+    //        {
+    //            if(!gridArray[x,y].hasResources)
+    //            {
+    //                gridArray[x, y].intensity += 100 / (Mathf.Abs(tile.x - x) + Mathf.Abs(tile.y - y)) / 2;
 
 
+    //                var tileRenderer = gridArray[x, y].tile.GetComponent<Renderer>();
 
-    void AddExtraSpots()
-    {
+    //                Color currentColor = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color");
 
-    }
+    //                float currR = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color").r;
+    //                float currG = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color").g;
+    //                float currB = gridArray[x, y].GetComponent<Renderer>().material.GetColor("_Color").b;
 
+
+    //                float tempR = 1.0f / (Mathf.Abs(tile.x - x) + Mathf.Abs(tile.y - y)) / 2;
+    //                float tempG = 0;
+    //                float tempB = 1.0f / (Mathf.Abs(tile.x - x) + Mathf.Abs(tile.y - y)) / 2;
+
+    //                tileRenderer.material.SetColor("_Color", new Color(currR - tempR, currG - tempG, currB - tempB));
+    //            }
+
+    //        }
+    //    }
+
+    //}
+    //public void SetUpCoverGrid()
+    //{
+    //    if (coverTileGrid != null)
+    //    {
+    //        for (int x = 0; x < coverTileGrid.GetLength(0); x++)
+    //        {
+    //            for (int y = 0; y < coverTileGrid.GetLength(1); y++)
+    //            {
+    //                Destroy(coverTileGrid[x, y].tile);
+    //                coverTileGrid[x, y] = null;
+    //            }
+    //        }
+    //        coverTileGrid = null;
+    //    }
+
+
+    //    coverTileGrid = new CoverTile[width, height];
+
+    //    int counter = 0;
+
+    //    for (int x = 0; x < coverTileGrid.GetLength(0); x++)
+    //    {
+    //        for (int y = 0; y < coverTileGrid.GetLength(1); y++)
+    //        {
+
+    //            GameObject temp = Instantiate(coverTile, new Vector3((x - (height / 2) + 0.5f)*0.5f, (y - (width / 2) + 0.5f) * 0.5f, 0), Quaternion.identity);
+    //            coverTileGrid[x, y] = temp.GetComponent<CoverTile>();
+    //            coverTileGrid[x, y].id = counter;
+    //            counter++;
+
+    //            coverTileGrid[x, y].x = x;
+    //            coverTileGrid[x, y].y = y;
+    //        }
+    //    }
+    //}
 }
 
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,8 +6,144 @@ public class Tile : MonoBehaviour
 {
     public int x = 0;
     public int y = 0;
-    public GameObject tile;
+    GameObject tile;
     public int id = 0;
     public int intensity = 0;
     public bool hasResources = false;
+    public bool isSelected;
+
+    private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    private SpriteRenderer render;
+    private static Tile previousSelected;
+    private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+    private bool matchFound = false;
+
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+    void OnMouseDown()
+    {
+        
+        if (render.sprite == null || GridController.instance.IsShifting)
+        {
+            return;
+        }
+
+        if (isSelected)
+        { 
+            DeSelect();
+        }
+        else
+        {
+            if (previousSelected == null)
+            { 
+                Select();
+                
+            }
+                else
+                {
+                    SwapSprite(previousSelected.render);
+                    previousSelected.DeSelect();
+                }
+            
+        }
+    }
+
+
+    private void Select()
+    {
+        isSelected = true;
+        render.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Tile>();
+    }
+
+    private void DeSelect()
+    {
+        render.color = Color.white;
+        isSelected = false;
+        previousSelected = null;
+    }
+
+    public void SwapSprite(SpriteRenderer render2)
+    { 
+        if (render.sprite == render2.sprite)
+        {
+            
+            return;
+        }
+        
+        Sprite tempSprite = render2.sprite; 
+        render2.sprite = render.sprite; 
+        render.sprite = tempSprite; 
+        //SFXManager.instance.PlaySFX(Clip.Swap);
+    }
+
+    private GameObject GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+    private List<GameObject> GetAllAdjacentTiles()
+    {
+        List<GameObject> adjacentTiles = new List<GameObject>();
+        for (int i = 0; i < adjacentDirections.Length; i++)
+        {
+            adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+        }
+        return adjacentTiles;
+    }
+
+    private List<GameObject> FindMatch(Vector2 castDir)
+    { // 1
+        List<GameObject> matchingTiles = new List<GameObject>(); // 2
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir); // 3
+        while (hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == render.sprite)
+        { // 4
+            matchingTiles.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, castDir);
+        }
+        return matchingTiles; // 5
+    }
+
+    private void ClearMatch(Vector2[] paths) // 1
+    {
+        List<GameObject> matchingTiles = new List<GameObject>(); // 2
+        for (int i = 0; i < paths.Length; i++) // 3
+        {
+            matchingTiles.AddRange(FindMatch(paths[i]));
+        }
+        if (matchingTiles.Count >= 2) // 4
+        {
+            for (int i = 0; i < matchingTiles.Count; i++) // 5
+            {
+                matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
+            }
+            matchFound = true; // 6
+        }
+    }
+
+
+    public void ClearAllMatches()
+    {
+        if (render.sprite == null)
+            return;
+
+        ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+        ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+        if (matchFound)
+        {
+            render.sprite = null;
+            matchFound = false;
+            //SFXManager.instance.PlaySFX(Clip.Clear);
+        }
+    }
+
+
 }
